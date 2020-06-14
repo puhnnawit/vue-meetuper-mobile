2ea2296bd536b72336311c6a31b79e3d8534aa8b
noppakrit <noppakrit.t@windowslive.com>
2020-06-14 22:44:01 +0700

    create components

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,16 +1,14 @@
 <template>
-  <!-- <view class="container"> -->
-    <text class="text-color-primary">{{title}}</text>
-  <!-- </view> -->
+    
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      title: 'Hello World'
+    data() {
+        return {
+            message : 'Welcome to my application'
+        }
     }
-  }  
 }
 </script>
 
